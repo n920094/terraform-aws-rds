77fdb43b5ce1a3991bdd2d942e47032a89e27411
Minoru Nakabayashi <nakabayashi.m@teldevice.co.jp>
2024-11-07 20:13:33 +0900

    cidr modify

--- main.tf
+++ main.tf
@@ -41,8 +41,8 @@ resource "aws_security_group" "rds" {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-  #  cidr_blocks = ["192.80.0.0/16"]
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["192.80.0.0/16"]
+    #cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
